c23ec48add00fbbd30c5a0e3f9d21438f61604ac
szewczukk <dmitro.szewczuk@gmail.com>
2023-10-14 15:43:38 +0200

    Add next and previous slide buttons

--- lab2/main.js
+++ lab2/main.js
@@ -1,39 +1,61 @@
 const slides = document.querySelector('.slides');
 const stopButton = document.querySelector('.stop');
 const controls = document.querySelector('.controls');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+const identifier = document.querySelector('#identifier');
 
 let currentSlide = 1;
 let stopped = false;
 let ref;
 
-function carousel() {
-	ref = setInterval(() => {
-		const prevSlide = currentSlide;
-		currentSlide++;
-		if (currentSlide > 6) {
-			currentSlide = 1;
-		}
+function updateSlide(prevSlide, currentSlide) {
+	slides.classList.remove(`slide${prevSlide}`);
+	slides.classList.add(`slide${currentSlide}`);
 
-		slides.classList.remove(`slide${prevSlide}`);
-		slides.classList.add(`slide${currentSlide}`);
-	}, 3000);
+	identifier.textContent = currentSlide + 1;
+}
+
+function nextSlide() {
+	const prevSlide = currentSlide;
+	currentSlide++;
+	if (currentSlide > 6) {
+		currentSlide = 1;
+	}
+
+	updateSlide(prevSlide, currentSlide);
+}
+
+function previousSlide() {
+	const prevSlide = currentSlide;
+	currentSlide--;
+	if (currentSlide === 0) {
+		currentSlide = 6;
+	}
+
+	updateSlide(prevSlide, currentSlide);
+}
+
+function carousel() {
+	ref = setInterval(nextSlide, 3000);
 }
 
 function stopCarousel() {
-	if (!stopped) {
-		clearInterval(ref);
-		stopped = true;
-		stopButton.textContent = 'play';
-		return;
-	}
-
-	carousel();
-	stopButton.textContent = 'stop';
+	clearInterval(ref);
+	stopped = true;
+	stopButton.textContent = 'play';
 }
 
 carousel();
 
-stopButton.addEventListener('click', stopCarousel);
+stopButton.addEventListener('click', () => {
+	if (!stopped) {
+		stopCarousel();
+		return;
+	}
+	carousel();
+	stopButton.textContent = 'stop';
+});
 
 [...controls.children].forEach((control, index) => {
 	control.addEventListener('click', () => {
@@ -41,7 +63,17 @@ stopButton.addEventListener('click', stopCarousel);
 			slides.classList.remove(`slide${i}`);
 		}
 
-		slides.classList.add(`slide${index}`);
+		updateSlide(0, index);
 		stopCarousel();
 	});
+});
+
+nextButton.addEventListener('click', () => {
+	stopCarousel();
+	nextSlide();
+});
+
+previousButton.addEventListener('click', () => {
+	stopCarousel();
+	previousSlide();
 });
