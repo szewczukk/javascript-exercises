91bd512d2059769b479c1433a0ede39f0f32d370
szewczukk <dmitro.szewczuk@gmail.com>
2024-01-12 00:17:19 +0100

    Add contenteditable

--- lab4/script.js
+++ lab4/script.js
@@ -1,28 +1,62 @@
-const notes = ['Note #1', 'Note #2'];
+const notesStore = [
+	{ id: crypto.randomUUID(), title: 'Note #1', body: 'Hello from Note #1' },
+	{ id: crypto.randomUUID(), title: 'Note #2', body: 'Hello from Note #2' },
+];
+let focusedNote = notesStore[0];
 
 const asideFilter = document.querySelector('#aside__filter');
 const asideList = document.querySelector('.aside__list');
+const noteEditable = document.querySelector('.note__editable');
 
-for (const note of notes) {
-	const item = document.createElement('li');
-
-	item.textContent = note;
-
-	asideList.appendChild(item);
-}
-
-asideFilter.addEventListener('input', (e) => {
-	const newNotes = [];
-	const filter = e.target.value;
-	const filteredNotes = notes.filter((note) => note.includes(filter));
-
-	for (const note of filteredNotes) {
-		const item = document.createElement('li');
-
-		item.textContent = note;
-
-		newNotes.push(item);
+function itemOnClick(e) {
+	const items = document.querySelectorAll('.aside__item');
+	for (const secondItem of items) {
+		if (secondItem.getAttribute('id') === focusedNote.id) {
+			secondItem.classList.remove('aside__item--focused');
+		}
 	}
 
-	asideList.replaceChildren(...newNotes);
+	e.target.classList.add('aside__item--focused');
+	focusedNote = notesStore.find((n) => n.id === e.target.getAttribute('id'));
+
+	noteEditable.innerHTML = focusedNote.body;
+}
+
+function updateAsideItems(notes) {
+	const items = [];
+
+	for (const note of notes) {
+		const item = document.createElement('li');
+		item.classList.add('aside__item');
+		item.setAttribute('id', note.id);
+		item.addEventListener('click', itemOnClick);
+
+		item.textContent = note.title;
+
+		if (note === focusedNote) {
+			item.classList.add('aside__item--focused');
+		}
+
+		items.push(item);
+	}
+
+	asideList.replaceChildren(...items);
+}
+
+updateAsideItems(notesStore);
+noteEditable.innerHTML = focusedNote.body;
+
+asideFilter.addEventListener('input', (e) => {
+	const filter = e.target.value;
+	const filteredNotes = notesStore.filter((note) =>
+		note.title.includes(filter),
+	);
+
+	updateAsideItems(filteredNotes);
 });
+
+noteEditable.addEventListener('input', (e) => {
+	focusedNote.body = e.target.innerHTML;
+});
+
+noteEditable.focus();
