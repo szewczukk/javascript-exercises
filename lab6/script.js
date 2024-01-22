d62ce4efde493d0aa5a945823d196ad4fccfb4e1
szewczukk <dmitro.szewczuk@gmail.com>
2024-01-22 12:37:18 +0100

    Add clicking on a ball to lab6

--- lab6/script.js
+++ lab6/script.js
@@ -1,8 +1,5 @@
 class Ball {
-	/** @param balls {Ball[]} */
-	constructor(balls) {
-		this.balls = balls;
-
+	constructor() {
 		this.posX = Math.random() * 790;
 		this.posY = Math.random() * 590;
 
@@ -23,12 +20,22 @@ class Ball {
 			this.directionY *= -1;
 		}
 
-		for (const ball of this.balls) {
-			const d = Math.sqrt(
-				Math.pow(ball.posX - this.posX, 2) + Math.pow(ball.posY - this.posY, 2),
-			);
+		this.connectBalls();
 
-			if (d < 75) {
+		ctx.beginPath();
+		ctx.arc(this.posX, this.posY, 5, 0, Math.PI * 2, true);
+
+		ctx.fillStyle = 'red';
+		ctx.fill();
+	}
+
+	connectBalls() {
+		for (const ball of balls) {
+			if (ball === this) {
+				return;
+			}
+
+			if (this.distance(ball) < distance) {
 				ctx.beginPath();
 				ctx.moveTo(this.posX, this.posY);
 				ctx.lineTo(ball.posX, ball.posY);
@@ -36,12 +43,13 @@ class Ball {
 				ctx.stroke();
 			}
 		}
+	}
 
-		ctx.beginPath();
-		ctx.arc(this.posX, this.posY, 5, 0, Math.PI * 2, true);
-
-		ctx.fillStyle = 'red';
-		ctx.fill();
+	distance(otherBall) {
+		return Math.sqrt(
+			Math.pow(otherBall.posX - this.posX, 2) +
+				Math.pow(otherBall.posY - this.posY, 2),
+		);
 	}
 }
 
@@ -51,15 +59,19 @@ const canvas = document.querySelector('#canvas');
 const startButton = document.querySelector('#start');
 const restartButton = document.querySelector('#reset');
 
+const distanceRange = document.querySelector('#distance');
+
 const ctx = canvas.getContext('2d');
 
+/** @type{Ball[]} */
+let balls = [];
+let distance = 50;
 let state = 'paused';
 
-let balls = [];
 function restart() {
 	balls = [];
 	for (let i = 0; i < 50; i++) {
-		balls.push(new Ball(balls));
+		balls.push(new Ball());
 	}
 }
 
@@ -97,4 +109,28 @@ restartButton.addEventListener('click', () => {
 
 	restart();
 	draw();
+});
+
+distanceRange.addEventListener('input', function () {
+	distance = this.value;
+});
+
+canvas.addEventListener('click', function (e) {
+	const mousePos = { posX: e.offsetX, posY: e.offsetY };
+
+	const clickedBalls = balls.filter((ball) => {
+		return ball.distance(mousePos) <= 5;
+	});
+
+	if (clickedBalls.length === 0) {
+		return;
+	}
+
+	for (const ball of clickedBalls) {
+		balls = balls.filter((b) => b !== ball);
+	}
+
+	for (let i = 0; i < 2; i++) {
+		balls.push(new Ball());
+	}
 });
